2486511e0c88b8ffc71a7cb779a978661fa5428e
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-09-24 12:47:21 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from '../styles/Global';
+import '../index.css';
+
+const Button = ({ assetUrl, link }) => {
+  return (
+    <div 
+      className={`${styles.btnBlack}`}
+      onClick={() => window.open(link, "_blank")}
+    >
+      <img src={assetUrl} alt="expo_icon" className={`${styles.btnIcon} w-48 h-48`} />
+      <div className="flex flex-col ml-4">
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+      </div>
+    </div>
+  )
+}
+
+export default Button
